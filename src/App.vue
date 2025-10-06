f5386b85b33c28c8c740b026b60d6f455b80c1b5
Fernando Herrera <fernando.herrera85@gmail.com>
2025-10-06 13:32:02 -0400

    refactor: update App.vue to include ShoppingCart component and restructure template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
-    <h1>Hola Mundo</h1>
+    <h2>Mi carrito de compras</h2>
+
+    <ShoppingCart :name="product.name" :quantity="product.quantity" />
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ShoppingCart from './components/ShoppingCart.vue';
+
+const product = ref({
+  name: 'Camiseta',
+  quantity: 10,
+});
+</script>
